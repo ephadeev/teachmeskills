e6c41502373ea812725b0515b34af8f97b889986
Evgeniy Phadeev <57875528+ephadeev@users.noreply.github.com>
2020-02-05 19:50:46 +0300

    Create main.js

---
+++ home-work-1/main.js
@@ -0,0 +1,23 @@
+var car1 = {
+    model: "Tesla Model S",
+    NumberOfDoors: 5,
+    isElectric: true,
+    owner: {
+        firstName: "Elon",
+        lastName: "Musk",
+    },
+};
+
+var car2 = {
+    model: "Lamborghini Diablo",
+    NumberOfDoors: 2,
+    isElectric: false,
+    owner: {
+        firstName: "Steven",
+        lastName: "Jobs",
+    },
+};
+
+var cars = [car1, car2];
+
+console.log(cars);
